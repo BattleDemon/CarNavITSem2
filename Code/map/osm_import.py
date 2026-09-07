0dc9ce16a7111d564fba45398749ba3264ea0438
DexterH <DexterHart0@outlook.com>
2026-09-07 15:07:25 +1000

    working on the project

--- Code/map/osm_import.py
+++ Code/map/osm_import.py
@@ -6,7 +6,7 @@ class OSMImporter:
     def __init__(self):
         # Finds the parent directory of this file (since the map is in the same dir)
         self.map_dir = Path(__file__).parent
-        self.osm_file = self.map_dir / "weston_creek.osm.pbf"
+        self.osm_file = self.map_dir / "WEST.osm.pbf"
 
         self.osm = OSM(self.osm_file)
 
@@ -20,6 +20,8 @@ class OSMImporter:
 
         print("Length")
         print(len(self.road_network))
+
+        print(self.road_network)
 
 
 osmimport = OSMImporter()
